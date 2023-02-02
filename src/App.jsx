675e87be07aa97421bc52938c72666404cbdb920
BHEET <krkbheet@gmail.com>
2023-02-02 21:49:11 +0100

    setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Hello World</h1>
+    </div>
+  );
+}
