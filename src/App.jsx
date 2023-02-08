80ab6fc9c12edd50a4ae63e388080da9bde60dfe
BHEET <krkbheet@gmail.com>
2023-02-08 18:26:35 +0100

    update css, html

--- src/App.jsx
+++ src/App.jsx
@@ -9,27 +9,33 @@ export default function App() {
     <div className="wrapper">
       <Navbar />
       <main>
-        <h1 className="mainTitle">
-          Solana <br />
-          Competitive
-          <br />
-          Chess
-        </h1>
-        <h2>What is Shess?</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores
-          repudiandae vitae nobis perferendis eum unde officia rem accusantium
-          voluptas praesentium, reiciendis sunt, magni ipsam a provident optio
-          ea omnis suscipit.
-        </p>
-
-        <h2>Modes</h2>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla quod
-          libero quae? Magnam, vero? Aliquid, ab? Corrupti commodi vero, veniam
-          dolores mollitia ratione impedit maxime, minima ipsam autem magnam
-          repellendus.
-        </p>
+        <div className="front">
+          <h1 className="mainTitle">
+            Solana <br />
+            Web3 Chess
+          </h1>
+          <button className="startButton">Start</button>
+        </div>
+        <div className="articles">
+          <article>
+            <h2>What is Shess?</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Asperiores repudiandae vitae nobis perferendis eum unde officia
+              rem accusantium voluptas praesentium, reiciendis sunt, magni ipsam
+              a provident optio ea omnis suscipit.
+            </p>
+          </article>
+          <article>
+            <h2>Modes</h2>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla
+              quod libero quae? Magnam, vero? Aliquid, ab? Corrupti commodi
+              vero, veniam dolores mollitia ratione impedit maxime, minima ipsam
+              autem magnam repellendus.
+            </p>
+          </article>
+        </div>
       </main>
       <Footer />
     </div>
