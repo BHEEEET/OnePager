4231775558e482803d32a47439f0f8fc2574b2c2
BHEET <krkbheet@gmail.com>
2023-02-15 22:01:27 +0100

    update footer | box-shadow

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,16 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
-      <h2>Shess</h2>
       <a href="https://www.discord.gg/uusf6MrjHz">
         <img src="icons/discord.svg" width={30} alt="Discord" />
       </a>
       <a href="https://www.discord.gg/uusf6MrjHz">
-        <img src="icons/twitter.svg" width={30} alt="Discord" />
+        <img src="icons/twitter.svg" width={30} alt="Twitter" />
       </a>
+      <a href="https://www.discord.gg/uusf6MrjHz">
+        <img src="icons/file-text.svg" width={30} alt="Whitepaper" />
+      </a>
+      <span>Shess</span>
     </footer>
   );
 }
