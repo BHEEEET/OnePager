6201999b87cf779edbf2aeaadee1980b13349365
BHEET <krkbheet@gmail.com>
2023-02-15 22:23:35 +0100

    github icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,11 +6,14 @@ export default function Footer() {
       <a href="https://www.discord.gg/uusf6MrjHz">
         <img src="icons/discord.svg" width={30} alt="Discord" />
       </a>
-      <a href="https://www.discord.gg/uusf6MrjHz">
+      <a href="https://www.twitter.com/shesssolana">
         <img src="icons/twitter.svg" width={30} alt="Twitter" />
       </a>
-      <a href="https://www.discord.gg/uusf6MrjHz">
+      <a href="https://krk-1.gitbook.io/shess/">
         <img src="icons/file-text.svg" width={30} alt="Whitepaper" />
+      </a>
+      <a href="https://krk-1.gitbook.io/shess/">
+        <img src="icons/github.svg" width={30} alt="Github" />
       </a>
       <span>Shess</span>
     </footer>
