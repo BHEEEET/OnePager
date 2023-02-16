ad8fa0c8373e52661f07b96caec3dbc1fbf624fe
BHEET <krkbheet@gmail.com>
2023-02-16 19:48:41 +0100

    test background

--- src/App.jsx
+++ src/App.jsx
@@ -42,30 +42,6 @@ export default function App() {
               a provident optio ea omnis suscipit.
             </p>
           </article>
-          <div className="container">
-            <div className="card">
-              <div className="imgBx">
-                <img src="./build/Logo_Light.svg" />
-              </div>
-              <div className="contentBx">
-                <h2>Nike Shoes</h2>
-                <div className="size">
-                  <h3>Size :</h3>
-                  <span>7</span>
-                  <span>8</span>
-                  <span>9</span>
-                  <span>10</span>
-                </div>
-                <div className="color">
-                  <h3>Color :</h3>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                </div>
-                <a href="#">Buy Now</a>
-              </div>
-            </div>
-          </div>
 
           <article className="contentBx">
             <h2>Modes</h2>
