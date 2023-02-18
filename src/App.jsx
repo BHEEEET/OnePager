06cad896bbdbb8a797ff78e8aa805280a4cdcfb3
BHEET <krkbheet@gmail.com>
2023-02-18 16:39:56 +0100

    update peopleCounter

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,20 @@ export default function App() {
 
   useEffect(() => {
     let interval = null;
+    let delay = 3;
+
     if (count < 550) {
+      delay = 3;
+    } else if (count < 650) {
+      delay = 10;
+    } else {
+      delay = 100;
+    }
+
+    if (count < 700) {
       interval = setInterval(() => {
         setCounter((count) => count + 1);
-      }, 3);
-    } else if (count < 670) {
-      interval = setInterval(() => {
-        setCounter((count) => count + 1);
-      }, 10);
-    } else if (count < 700) {
-      interval = setInterval(() => {
-        setCounter((count) => count + 1);
-      }, 100);
+      }, delay);
     } else {
       clearInterval(interval);
     }
@@ -29,64 +31,66 @@ export default function App() {
   }, [count]);
 
   return (
-    <div className="wrapper">
-      <Navbar />
-      <main>
-        <div className="front">
-          <h1 className="mainTitle">Solana Chess</h1>
-          <p className="mainText">Grizzlython</p>
-          <button className="startButton">
-            <img src="icons/play-fill.svg" />
-            <span className="buttonText">Soon™</span>
-          </button>
-        </div>
-        <div className="mostPlayed">
-          <h2> One of the most played game.</h2>
-          <div className="peopleNum">{count}M+</div>
-          <div className="people">PEOPLE</div>
-          <br />
-          in the whole world
-          <br /> that have played chess
-        </div>
-        <div className="articles">
-          <article>
-            <h2>Hackathon</h2>
-            <img className="grizz" src="Logo_Light.svg" />
-          </article>
-          <article>
-            <h2>What is Shess?</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Asperiores repudiandae vitae nobis perferendis eum unde officia
-              rem accusantium voluptas praesentium, reiciendis sunt, magni ipsam
-              a provident optio ea omnis suscipit.
-            </p>
-          </article>
+    <React.StrictMode>
+      <div className="wrapper">
+        <Navbar />
+        <main>
+          <div className="front">
+            <h1 className="mainTitle">Solana Chess</h1>
+            <p className="mainText">Grizzlython</p>
+            <button className="startButton">
+              <img src="icons/play-fill.svg" />
+              <span className="buttonText">Soon™</span>
+            </button>
+          </div>
+          <div className="mostPlayed">
+            <h2> One of the most played game.</h2>
+            <div className="peopleNum">{count}M+</div>
+            <div className="people">PEOPLE</div>
+            <br />
+            in the whole world
+            <br /> that have played chess
+          </div>
+          <div className="articles">
+            <article>
+              <h2>Hackathon</h2>
+              <img className="grizz" src="Logo_Light.svg" />
+            </article>
+            <article>
+              <h2>What is Shess?</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Asperiores repudiandae vitae nobis perferendis eum unde officia
+                rem accusantium voluptas praesentium, reiciendis sunt, magni
+                ipsam a provident optio ea omnis suscipit.
+              </p>
+            </article>
 
-          <article className="contentBx">
-            <h2>Modes</h2>
-            <div className="PvP modes">
-              <h3>Player vs Player</h3>
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla
-                quod libero quae? Magnam, vero? Aliquid, ab? Corrupti commodi
-                vero, veniam dolores mollitia ratione impedit maxime, minima
-                ipsam autem magnam repellendus.
-              </p>
-            </div>
-            <div className="DvD modes">
-              <h3>DAO vs DAO</h3>
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla
-                quod libero quae? Magnam, vero? Aliquid, ab? Corrupti commodi
-                vero, veniam dolores mollitia ratione impedit maxime, minima
-                ipsam autem magnam repellendus.
-              </p>
-            </div>
-          </article>
-        </div>
-      </main>
-      <Footer />
-    </div>
+            <article className="contentBx">
+              <h2>Modes</h2>
+              <div className="PvP modes">
+                <h3>Player vs Player</h3>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Nulla quod libero quae? Magnam, vero? Aliquid, ab? Corrupti
+                  commodi vero, veniam dolores mollitia ratione impedit maxime,
+                  minima ipsam autem magnam repellendus.
+                </p>
+              </div>
+              <div className="DvD modes">
+                <h3>DAO vs DAO</h3>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Nulla quod libero quae? Magnam, vero? Aliquid, ab? Corrupti
+                  commodi vero, veniam dolores mollitia ratione impedit maxime,
+                  minima ipsam autem magnam repellendus.
+                </p>
+              </div>
+            </article>
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </React.StrictMode>
   );
 }
